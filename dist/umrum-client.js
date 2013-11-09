1317146bcd209ef897fff454f009f971829f7f42
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-09 18:43:25 -0300

    Creating timed ping requests to app server

--- dist/umrum-client.js
+++ dist/umrum-client.js
@@ -1,5 +1,8 @@
+/* UMRUM client library */
 
-(function(win){
+(function(win, doc){
+    var _server_ping = 'http://frontend-bahia.2013.nodeknockout.com/ping';
+    var _server_ping_timeout = 10000;
     var lib = win['umrum'] =  win['umrum'] || {};
 
     //http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript#answer-2117523
@@ -12,4 +15,21 @@
 
     lib.host = win.location.host.replace(/^www\./i, '');
     lib.path = win.location.pathname.replace(/([^\/])/i, '/$1');
-})(window);
+
+    var ping_server = function(){
+      var img = doc.createElement('img');
+      img.src = [
+        _server_ping,
+        "?uid=",
+        encodeURIComponent(lib.uid),
+        "&host=",
+        encodeURIComponent(lib.host),
+        "&path=",
+        encodeURIComponent(lib.path)
+      ].join('');
+      img.height = img.width = 1;
+      doc.body.appendChild(img);
+      setTimeout(ping_server, _server_ping_timeout);
+    };
+    ping_server();
+})(window, document);
