3bb698d4c48b5f8dbbed494b50a82331084ebd04
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-09 16:26:46 -0300

    Creating umrum client lib; UUID is being generated for each script load

---
+++ dist/umrum-client.js
@@ -0,0 +1,12 @@
+
+(function(win){
+    var lib = win['umrum'] =  win['umrum'] || {};
+
+    //http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript#answer-2117523
+    lib.uid = function() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    };
+})(window);
