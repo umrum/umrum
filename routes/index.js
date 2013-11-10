f08c33840e3039c7348a85aae098a8735fb5b8c8
Ricardo Torres <ricardo@rictorres.com.br>
2013-11-10 20:35:12 -0200

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,23 @@ var app = require('../config/app');
 app.get('/', function(req, res) {
     res.render('index.html');
 });
+
+app.get('/getting-started', function(req, res) {
+    res.render('getting-started.html');
+});
+
+app.get('/documentation', function(req, res) {
+    res.render('documentation.html');
+});
+
+app.get('/support', function(req, res) {
+    res.render('support.html');
+});
+
+app.get('/terms-of-service', function(req, res) {
+    res.render('terms-of-service.html');
+});
+
+app.get('/privacy-policy', function(req, res) {
+    res.render('privacy-policy.html');
+});
