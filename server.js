bb48b4a7bd9aa8dd3ef2603722f5b763375e393f
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2013-11-11 13:53:57 -0500

    Removing NKO references from index.html and server.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 /* global require */
 
-// https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
-require('nko')('jMRluCJSxECo879y');
-
 var routes = ['index', 'ping'];
 for (var i = routes.length - 1; i >= 0; i--) {
     require('./routes/' + routes[i]);
