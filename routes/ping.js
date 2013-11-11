97233ab0291bfdacbdfc184ff2913cdc1adfb5ac
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-10 21:03:20 -0300

    Remove clear expire timeout handler

--- routes/ping.js
+++ routes/ping.js
@@ -10,16 +10,9 @@ app.get('/ping', function(req, res) {
 
     api.registerPageView(uid, host, path);
 
-    // I dont know how acess this expires. Could be a set in redis with
-    // redis expires. The code below is just a representation of how
-    // expires should works
-    if (expires) {
-        clearTimeout(expires);
-    }
-
-    var expires = setTimeout(function() {
+    setTimeout(function() {
         api.removePageView(host, path);
-    })
+    });
 
     res.send([uid, host, path].join(' - '));
 });
