c5c63bfc7782eb960d4906b86fd8e5dd27ee159d
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-11 00:36:53 -0300

    Improving interaction with redis to store users access data

--- ext/redis.js
+++ ext/redis.js
@@ -2,57 +2,85 @@
 
 var redisclient = require('../config/redisclient');
 
-var getHostInfo = function(host) {
+var _MAX_TOPPAGES = 10;
+
+var _toppages_key(host) {
+    return 'toppages-'+host;
+}
+
+var getHostInfo = function(host, callback) {
     /*
     This function should returns a map with the current visitors of
     the host and the top pages.
-    return {
+
+    @callback will receive hostinfo map as parameter
+
+    hostinfo map: {
         currentVisits: 123,
-        topPages: [{
-            'currentVisits': 10,
-            'title': 'Lorem Ipsum'
-        }]
+        topPages: [[path, currVisits], ...]
     }
     */
-    var currentVisits = redisclient.get(host);
-    var hostHash = host;
-    var topPages = redisclient.zrangebyscore(hostHash, '-inf +inf', 'WITHSCORES', 'LIMIT 0 10');
 
-    var formatTopPages = function() {
-        var formatted = [];
-        for (var i = 0; i < topPages.length; i++) {
-            // We don't have a hashLib defined
-            var path = hashLib.decript(topPages[i]['pathHash']);
-            formatted.push({
-                'currentVisits': topPages[i]['count'],
-                'path': path
-            });
-        };
-        return formatted;
-    }
-    return {
-        currentVisits: currentVisits,
-        topPages: formatTopPages
-    }
+    hostinfo = {}
+    redisclient.hget(host, 'curr_visits', function(err, result){
+        if ( err ) throw err;
+        if ( !result ) return hostinfo;
+
+        hostinfo.currentVisits = result;
+        redisclient.zrevrangebyscore(
+            _toppages_key(host),
+            '-inf', '+inf',
+            'WITHSCORES',
+            'LIMIT', 0, _MAX_TOPPAGES,
+            function(err, result) {
+                if ( err ) throw err;
+                if ( !result ) return hostinfo;
+
+                hostinfo.topPages = [];
+                for (var i=0; i<result.length; i+=2) {
+                    hostinfo.topPages.push([result[i], result[i+1]]);
+                }
+
+                callback(hostinfo);
+            }
+        );
+    });
 }
 
 var registerPageView = function(active_user) {
     /*
-    This function should increment the number of visitiors of host
-    and adds the pam of this request (stores path) with expires:
+    This function must save the active user and increment its host attributes
+    like current_visits total and for its path
 
-    - host.currentVisits += 1  # with expire 10s
-    - paths.incr(path)  # with expire 10s
+    - active_user : JSON {
+      - host: host being accessed
+      - path: path being accessed
+      - title: meta title for the path
+      - uuid: user ID
+    }
     */
-    var pathHash = hashLib.encript(active_user.path);
-    redisclient.incr(active_user.host);
-    // zincrby path 1 "path"
-    redisclient.zincrby(pathHash, 1, "path");
+
+    redisclient.hmset(active_user.uuid, active_user);
+    redisclient.hincrby(active_user.host, 'curr_visits', 1);
+    redisclient.zincrby(_toppages_key(active_user.host), 1, active_user.path);
 }
 
 var removePageView = function(active_user) {
-    redisclient.decr(active_user.host);
-    redisclient.zincrby(pathHash, -1, "path");
+    /*
+    This function must remove the active user and decrement its host attributes
+    like current_visits total and for its path
+
+    - active_user : JSON {
+      - host: host being accessed
+      - path: path being accessed
+      - title: meta title for the path
+      - uuid: user ID
+    }
+    */
+
+    redisclient.del(active_user.uuid);
+    redisclient.hincrby(active_user.host, 'curr_visits', -1);
+    redisclient.zincrby(_toppages_key(active_user.host), -1, active_user.path);
 }
 
 module.exports = {
