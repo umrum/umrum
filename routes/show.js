dae976e3ff8d8ea260acdaf63e94f6878d61336c
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-11 00:37:27 -0300

    Using the correct way to request data in event based env in show.js

--- routes/show.js
+++ routes/show.js
@@ -1,9 +1,10 @@
 /* global require */
 
 var app = require('../config/app');
-var redis = require('../ext/redis');
+var api = require('../ext/redis');
 
 app.get('/show/:host', function(req, res) {
-    var info = redis.getHostInfo(req.params.host);
-    res.render('show.html', info=info);
+    api.getHostInfo(req.params.host, function(info){
+      res.render('show.html', {'info': info});
+    });
 });
