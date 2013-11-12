0147c035495a5d47e2bd587e27d51e3f88fd0d2f
willmendesneto <willmendesneto@gmail.com>
2013-11-12 00:40:57 -0300

    Removing Facebook informations.

--- config/env.js
+++ config/env.js
@@ -13,11 +13,6 @@ module.exports = {  //require('config');
     MONGO_URI: 'mongodb://ds053858.mongolab.com:53858/umrum',
     models_path: path.normalize(path.join(__dirname, '../app/models')),
     controllers_path: path.normalize(path.join(__dirname, '../app/controllers')),
-    facebook: {
-        "clientID": "160994660777467",
-        "clientSecret": "8061f2ce997c225bef14e45ad37027ff",
-        "callbackURL": "http://localhost:8000/auth/facebook/callback"
-    },
     github: {
         "clientID": "b8a5184274772740060a",
         "clientSecret": "f64d75f0859934c8eb2c80959186e3e705b3d5f1",
