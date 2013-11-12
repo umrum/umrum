3e94744c96eb6256941ea52c9a621016c638a8f7
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-12 02:52:55 -0300

    Added tests for no results and error behavior of redisclient in getHostInfo

--- tests/ext/redis.js
+++ tests/ext/redis.js
@@ -48,5 +48,163 @@ describe('Tests the redis ext module', function(){
             sinon.assert.calledOnce(callback);
             sinon.assert.calledWith(callback, err, expectedHostInfo);
         });
+
+        it('no results hget', function() {
+            var host = 'testhost.com',
+                err = null,
+                currentVisits = null,
+                callback = sinon.mock();
+
+            var expectedHostInfo = {
+                currentVisits: null,
+                topPages: null,
+            };
+
+            var mockRedisCli = sinon.mock(redisclient);
+            mockRedisCli.expects("zrevrangebyscore").never();
+            mockRedisCli
+                .expects("hget")
+                .once().withArgs(host, 'curr_visits')
+                .callsArgWith(2, null, currentVisits)
+            ;
+
+            _api().getHostInfo(host, callback);
+
+            mockRedisCli.verify();
+            sinon.assert.calledOnce(callback);
+            sinon.assert.calledWith(callback, err, expectedHostInfo);
+        });
+
+        it('error in hget', function() {
+            var host = 'testhost.com',
+                err = {error: 'some error'},
+                callback = sinon.mock();
+
+            var expectedHostInfo = {
+                currentVisits: null,
+                topPages: null,
+            };
+
+            var mockRedisCli = sinon.mock(redisclient);
+            mockRedisCli.expects("zrevrangebyscore").never();
+            mockRedisCli
+                .expects("hget")
+                .once().withArgs(host, 'curr_visits')
+                .callsArgWith(2, err, null)
+            ;
+
+            _api().getHostInfo(host, callback);
+
+            mockRedisCli.verify();
+            sinon.assert.calledOnce(callback);
+            sinon.assert.calledWith(callback, err, expectedHostInfo);
+        });
+
+        it('no result in zrevrangebyscore', function() {
+            var host = 'testhost.com',
+                err = null,
+                currentVisits = '7',
+                topPages = null,
+                callback = sinon.mock();
+
+            var expectedHostInfo = {
+                'currentVisits': currentVisits,
+                'topPages': null
+            };
+
+            var mockRedisCli = sinon.mock(redisclient);
+            mockRedisCli
+                .expects("zrevrangebyscore")
+                .once().withArgs(
+                    'toppages-'+host,
+                    '+inf', '-inf',
+                    'WITHSCORES',
+                    'LIMIT', 0, 10
+                )
+                .callsArgWith(7, null, topPages)
+            ;
+            mockRedisCli
+                .expects("hget")
+                .once().withArgs(host, 'curr_visits')
+                .callsArgWith(2, null, currentVisits)
+            ;
+
+            _api().getHostInfo(host, callback);
+
+            mockRedisCli.verify();
+            sinon.assert.calledOnce(callback);
+            sinon.assert.calledWith(callback, err, expectedHostInfo);
+        });
+
+        it('empty result in zrevrangebyscore', function() {
+            var host = 'testhost.com',
+                err = null,
+                currentVisits = '7',
+                topPages = [],
+                callback = sinon.mock();
+
+            var expectedHostInfo = {
+                'currentVisits': currentVisits,
+                'topPages': []
+            };
+
+            var mockRedisCli = sinon.mock(redisclient);
+            mockRedisCli
+                .expects("zrevrangebyscore")
+                .once().withArgs(
+                    'toppages-'+host,
+                    '+inf', '-inf',
+                    'WITHSCORES',
+                    'LIMIT', 0, 10
+                )
+                .callsArgWith(7, null, topPages)
+            ;
+            mockRedisCli
+                .expects("hget")
+                .once().withArgs(host, 'curr_visits')
+                .callsArgWith(2, null, currentVisits)
+            ;
+
+            _api().getHostInfo(host, callback);
+
+            mockRedisCli.verify();
+            sinon.assert.calledOnce(callback);
+            sinon.assert.calledWith(callback, err, expectedHostInfo);
+        });
+
+        it('error in zrevrangebyscore', function() {
+            var host = 'testhost.com',
+                err = {error: 'some error'},
+                currentVisits = '7',
+                callback = sinon.mock();
+
+            var expectedHostInfo = {
+                'currentVisits': currentVisits,
+                'topPages': null
+            };
+
+            var mockRedisCli = sinon.mock(redisclient);
+            mockRedisCli
+                .expects("zrevrangebyscore")
+                .once().withArgs(
+                    'toppages-'+host,
+                    '+inf', '-inf',
+                    'WITHSCORES',
+                    'LIMIT', 0, 10
+                )
+                .callsArgWith(7, err, null)
+            ;
+            mockRedisCli
+                .expects("hget")
+                .once().withArgs(host, 'curr_visits')
+                .callsArgWith(2, null, currentVisits)
+            ;
+
+            _api().getHostInfo(host, callback);
+
+            mockRedisCli.verify();
+            sinon.assert.calledOnce(callback);
+            sinon.assert.calledWith(callback, err, expectedHostInfo);
+        });
     });
 });
