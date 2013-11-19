c14b0d412b343ba28751bd58bedb164f68251352
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-18 21:41:57 -0300

    Removing code references to Mongo URI

--- app/config/env.js
+++ app/config/env.js
@@ -5,17 +5,13 @@ var path = require('path');
 var _env = process.env.NODE_ENV || 'dev',
     _ipaddr = process.env.NODE_IP || '127.0.0.1',
     _port = process.env.NODE_PORT || 8000,
-    _mongo_uri = process.env.MONGO_URI || 'mongodb://localhost/umrum'
+    _mongoUri = process.env.MONGO_URI || 'mongodb://localhost/umrum'
 ;
 
 if ( process.env.OPENSHIFT_SECRET_TOKEN ) {
   _env = 'production';
   _port = process.env.OPENSHIFT_NODEJS_PORT;
   _ipaddr = process.env.OPENSHIFT_NODEJS_IP;
-}
-
-if ( _env === 'production' ) {
-    _mongo_uri = 'mongodb://umrum:UmR*m1pp@ds053858.mongolab.com:53858/umrum';
 }
 
 module.exports = {  //require('config');
@@ -26,7 +22,7 @@ module.exports = {  //require('config');
     assetsPath: path.normalize(path.join(__dirname, '../../assets/')),
     assetsURL: '/assets/',
     minifyOutput: true,
-    MONGO_URI: _mongo_uri,
+    MONGO_URI: _mongoUri,
     modelsPath: path.normalize(path.join(__dirname, '../models')),
     controllersPath: path.normalize(path.join(__dirname, '../controllers')),
     github: {
