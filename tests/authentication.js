b3ad125d73912f141745a1a5d39004fcdc0e660b
Rafael Verger <rafael@rafaelverger.com.br>
2013-11-19 00:20:58 -0300

    Fixing timeout exceeded in auth test

--- tests/authentication.js
+++ tests/authentication.js
@@ -1,45 +1,47 @@
-/* global require, console, describe, it, before, after */
+/* global require, describe, it, before, after */
 
 var assert = require('assert'),
+    mongoose = require('mongoose'),
+    env = require('../app/config/env'),
     User = require('../app/models/user')
 ;
 
 describe('Test the authentication module', function() {
 
-  before(function(done) {
-      done();
-  });
-
-  after(function(done) {
-      User.remove({oauthID: 12345, name: 'testy'}).exec();
-      done();
-  });
-
-  it('Save user credentials', function(done) {
-
-    var user = new User({
-      oauthID: 12345,
-      name: 'testy',
-      created: Date.now()
+    before(function(done) {
+        mongoose.connection.on('connected', done);
+        try{
+            this.timeout(3 * 1000);
+            mongoose.connect(env.MONGO_URI);
+        } catch(err){}
     });
 
-    user.save(function(error) {
-      if (error) {
-        console.log('error' + error.message);
-      }else{
-        console.log('no error');
-      }
-      done();
-    });
-
-  });
-
-  it('Find a user by username', function(done) {
-      User.findOne({ oauthID: 12345, name: "testy" }, function(err, user) {
-        assert.equal(user.name, 'testy');
-        assert.equal(user.oauthID, 12345);
+    after(function(done) {
+        User.remove({oauthID: 12345, name: 'testy'}).exec();
         done();
-      });
-  });
+    });
+
+    it('Save user credentials', function(done) {
+
+        var user = new User({
+          oauthID: 12345,
+          name: 'testy',
+          created: Date.now()
+        });
+
+        user.save(function(error) {
+            assert.equal(error, null);
+            done();
+        });
+
+    });
+
+    it('Find a user by username', function(done) {
+        User.findOne({ oauthID: 12345, name: "testy" }, function(err, user) {
+            assert.equal(user.name, 'testy');
+            assert.equal(user.oauthID, 12345);
+            done();
+        });
+    });
 
 });
