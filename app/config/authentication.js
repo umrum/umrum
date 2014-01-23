985559342302c9d10ebed02120822c1ff027f76b
Rafael Verger <rafael@rafaelverger.com.br>
2014-01-22 22:03:42 -0300

    Fixing coding standards

--- app/config/authentication.js
+++ app/config/authentication.js
@@ -1,4 +1,4 @@
-/* global require, process, module */
+/* global require, module */
 
 var User = require('../models/user'),
     GitHubStrategy = require('passport-github').Strategy
@@ -6,51 +6,49 @@ var User = require('../models/user'),
 
 module.exports = function(passport, env) {
 
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function (err, user) {
-      done(err, user);
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
     });
-  });
 
-  passport.use(new GitHubStrategy({
-      clientID: env.github.clientID,
-      clientSecret: env.github.clientSecret,
-      callbackURL: env.github.callbackURL
-    },
-    function(accessToken, refreshtoken, profile, done) {
-      process.nextTick(function () {
-        var query = User.findOne({ 'oauthID': profile.id, provider: 'github' });
-
-        query.exec( function (err, oldUser) {
-
-          if(oldUser) {
-              done(null, oldUser);
-              return;
-          }
-
-          var newUser = new User({
-              oauthID : profile.id,
-              name : profile.displayName,
-              email : profile.emails[0].value,
-              username : profile.username,
-              provider : 'github',
-              created: Date.now(),
-              github : profile._json
-          });
-
-          newUser.save(function(err) {
-              if(err) {
-                  throw err;
-              }
-              done(null, newUser);
-          });
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
         });
-      });
-    }
-  ));
+    });
 
+    passport.use(new GitHubStrategy(
+        {
+            clientID: env.github.clientID,
+            clientSecret: env.github.clientSecret,
+            callbackURL: env.github.callbackURL
+        },
+        function(accessToken, refreshtoken, profile, done) {
+            var query = User.findOne({
+                oauthID: profile.id,
+                provider: 'github'
+            });
+            query.exec( function (err, oldUser) {
+                if(oldUser) {
+                    done(null, oldUser);
+                    return;
+                }
+                var newUser = new User({
+                    oauthID : profile.id,
+                    name : profile.displayName,
+                    email : profile.emails[0].value,
+                    username : profile.username,
+                    provider : 'github',
+                    created: Date.now(),
+                    github : profile._json
+                });
+
+                newUser.save(function(err) {
+                    if(err) {
+                         throw err;
+                    }
+                    done(null, newUser);
+                });
+            });
+        }
+    ));
 };
