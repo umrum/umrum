2b1cc8513d0775ce215de9c88b9bb4cd826e041f
Rafael Verger <rafael@rafaelverger.com.br>
2014-03-25 03:54:26 -0300

    Fixing login redirects; force users go to /dashboard

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -3,9 +3,15 @@
 module.exports = function(app, passport){
     app.get('/signin', passport.authenticate('github', { scope: 'email' }));
     app.get('/signup', passport.authenticate('github', { scope: 'email' }));
-    app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect : '/error-404' }), function (req, res) {
-        res.redirect('/admin-index.html');
-    });
+    app.get(
+        '/auth/github/callback',
+        passport.authenticate('github', { failureRedirect : '/error-404' }),
+        function (req, res) {
+            console.log(req.isAuthenticated());
+            console.log(req.user);
+            res.redirect('/dashboard');
+        }
+    );
 
     app.get('/logout', function (req, res) {
         req.logOut();
