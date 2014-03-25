ae3c60a3fe9aa131c07438f513c98b5fe73f005b
Rafael Verger <rafael@rafaelverger.com.br>
2014-03-25 03:53:40 -0300

    Create admin-index separated from admin-view

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,15 +1,39 @@
 /* global require, module */
 
-var api = require('../ext/redis');
+var api = require('../ext/redis'),
+    Site = require('../models/site');
 
 module.exports = function(app){
-    app.get('/dashboard/:host', function(req, res) {
-        api.getHostInfo(req.params.host, function(err, info){
-            //TODO add error handler
-            res.render('admin-index.html', {
-                host: req.params.host,
-                data: info
+    app.get('/dashboard', function(req, res) {
+        if ( !req.isAuthenticated() ) {
+            res.redirect('/signin');
+        } else {
+            Site.find({creator: req.user.username}, function(err, docs) {
+                if ( err ) throw err;
+                docs = docs.map(function(e){ return e.host; });
+                res.render('admin-index.html', {
+                    user: req.user,
+                    title: req.user.username,
+                    sites: docs
+                });
             });
-        });
+        }
+
     });
+
+    app.get('/dashboard/:host', function(req, res) {
+        if ( !req.isAuthenticated() ) {
+            res.redirect('/signin');
+        } else {
+            api.getHostInfo(req.params.host, function(err, info){
+                //TODO add error handler
+                res.render('admin-view.html', {
+                    user: req.user,
+                    title: req.params.host,
+                    data: info
+                });
+            });
+        }
+    });
+
 };
