39aa1610f3c979218f4d2d44aedc697dabf6a73d
Rafael Verger <rafael@rafaelverger.com.br>
2014-03-25 03:46:02 -0300

    Create model for Site object

---
+++ app/models/site.js
@@ -0,0 +1,17 @@
+/* global require */
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema
+;
+
+/**
+ * User Schema
+ */
+var SiteSchema = new Schema({
+    host: String,
+    creator: String,
+    created: Date,
+    companies: []
+});
+
+module.exports = mongoose.model('Site', SiteSchema);
