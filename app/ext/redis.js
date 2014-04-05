1189e16e547eca8fdd88362437419b8e89878898
Rafael Verger <rafael@rafaelverger.com.br>
2014-04-05 12:02:42 -0300

    Removing urls without viewers from topPages

--- app/ext/redis.js
+++ app/ext/redis.js
@@ -38,10 +38,13 @@ var _lazy_api = {
                 function(err, result) {
                     if ( err || !result ) return callback(err, hostinfo);
 
-                    hostinfo.topPages = [];
-                    for (var i=0; i<result.length; i+=2) {
-                        hostinfo.topPages.push([result[i], result[i+1]]);
-                    }
+                    // result = [url, urlViwers, url2, url2Viewers...]
+                    hostinfo.topPages = result.map(function(item, idx, arr){
+                        if (idx%2) return null;
+                        return [item, arr[idx+1]];
+                    }).filter(function(item){
+                        return item && item[1];
+                    });
 
                     callback(err, hostinfo);
                 }
