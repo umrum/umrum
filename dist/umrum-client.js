d1d00bec56245260e97a9ed6c973481cdbfc3f2b
Rafael Verger <rafael@rafaelverger.com.br>
2014-04-16 14:14:12 -0300

    Skip ping server if no interactions has occurred in time window
    Events allowed as interaction: window scroll, body click and body
    children mouseover

--- dist/umrum-client.js
+++ dist/umrum-client.js
@@ -5,7 +5,21 @@
         _server_ping_timeout = 15 * 1000,
         lib = win._mrm = win._mrm || {},
         1rum_ckn = '__1rum',
+        addEvent = function(){},
         ping_fn;
+
+    // cross addEvent method
+    if(doc.addEventListener) {
+        addEvent = function (elem, type, handler, useCapture){
+            elem.addEventListener(type, handler, !!useCapture);
+        }
+    }
+    else if (doc.attachEvent) {
+        addEvent = function (elem, type, handler) {
+            type = "on" + type;
+            elem.attachEvent(type, function(){ handler.apply(elem, arguments) });
+        }
+    }
 
     doc.cookie.split(';').forEach(function(cookie){
         if (cookie.indexOf(1rum_ckn) == 0) {
@@ -24,30 +38,54 @@
     }
 
     ping_fn = function() {
-        lib.url = encodeURIComponent(win.location.href);
-        lib.title = encodeURIComponent(doc.title);
+        if ( lib.interaction ) {
+            lib.interaction = false;
+            lib.url = encodeURIComponent(win.location.href);
+            lib.title = encodeURIComponent(doc.title);
 
-        var imgId = "umrum_ping",
-            img = doc.getElementById(imgId),
-            _exists = img != null;
+            var imgId = "umrum_ping",
+                img = doc.getElementById(imgId),
+                _exists = img != null;
 
-        if ( !_exists ) {
-            img = doc.createElement('img');
-            img.id = imgId;
-            img.style.position = 'absolute';
-            img.style.top = img.style.left = '-1px';
-            img.height = img.width = 1;
-            doc.body.appendChild(img);
+            if ( !_exists ) {
+                img = doc.createElement('img');
+                img.id = imgId;
+                img.style.position = 'absolute';
+                img.style.top = img.style.left = '-1px';
+                img.height = img.width = 1;
+                doc.body.appendChild(img);
+            }
+            img.src = [
+                _server_ping,
+                "?uid=", lib.uid,
+                "&hostId=", lib.hostId,
+                "&url=", lib.url,
+                "&title=", lib.title,
+                "&t=", (+new Date)
+            ].join('');
         }
-        img.src = [
-            _server_ping,
-            "?uid=", lib.uid,
-            "&hostId=", lib.hostId,
-            "&url=", lib.url,
-            "&title=", lib.title,
-            "&t=", (+new Date)
-        ].join('');
-        setTimeout(ping_fn, _server_ping_timeout);
+        lib.ping_timeout = setTimeout(ping_fn, _server_ping_timeout);
     };
     ping_fn();
+
+    // adding page interaction interaction listeners
+    addEvent(win, 'scroll', function(){ lib.interaction = true; });
+    addEvent(doc.body, 'click', function(){ lib.interaction = true; });
+    var bodyChildren = Array.prototype.slice.call(doc.body.children, childIdx);
+    var childIdx = bodyChildren.length;
+    while (childIdx) {
+        addEvent(
+            bodyChildren[--childIdx],
+            'mouseover',
+            function(){ lib.interaction = true; }
+        );
+    }
+
+    // adding leave page listeners
+    win.onblur = function() { lib.interaction = false; };
+    win.onbeforeunload = function(){
+        // must clear timeout
+        lib.interaction = false;
+        clearTimeout(lib.ping_timeout);
+    }
 })(window, document);
