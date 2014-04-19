17cb0d03b24e3afd7a5030f61c81cab1787cd4ac
Rafael Verger <rafael@rafaelverger.com.br>
2014-04-19 03:42:52 -0300

    Refactoring server.js test
    Now testing each config step in server.js

--- tests/server.js
+++ tests/server.js
@@ -1,34 +1,182 @@
-/* global describe, it, before, after */
+/* global describe, it, before, __dirname */
 
 var assert = require('assert'),
-    nunjucks = require('nunjucks'),
-    server = require('../server'),
+    sinon = require('sinon'),
+    proxyquire = require('proxyquire'),
     env = require('../app/config/env'),
-    app = null
+    path = require('path')
 ;
 
-describe('Tests the server module', function(){
+describe('server.js', function(){
+    var io,
+        walker,
+        express_app,
+        srv_requires,
+        express_listening;
 
-    before(function(done) {
-        app = server.app;
-        done();
+    before(function() {
+        express_listening = Math.random();
+        express_app = {
+            use: sinon.spy(),
+            set: sinon.spy(),
+            engine: sinon.spy(),
+            listen: sinon.stub().returns(express_listening),
+            router: Math.random(),
+            locals: {}
+        };
+
+        io = {set: sinon.spy()};
+        walker = {
+            on: sinon.stub(),
+            walk: sinon.spy(),
+        };
+        walker.on.returns(walker);
+
+        srv_requires = {
+            'express': sinon.stub().returns(express_app),
+            'mongoose': {connect: sinon.spy()},
+            'nunjucks': {configure: sinon.spy(), render: 'render'},
+            'passport': {initialize: sinon.spy(), session: sinon.spy()},
+            'socket.io': {listen: sinon.stub().returns(io)},
+            'filewalker': sinon.stub().returns(walker),
+
+            './app/config/authentication': sinon.spy(),
+
+            './app/routes/ping': sinon.spy(),
+            './app/routes/index': sinon.spy(),
+            './app/routes/errors': sinon.spy(),
+            './app/routes/dashboard': sinon.spy(),
+            './app/routes/authentication': sinon.spy()
+        };
+
+        var express_methods = [
+            'json',
+            'logger',
+            'static',
+            'session',
+            'favicon',
+            'compress',
+            'urlencoded',
+            'cookieParser',
+        ];
+        for (var idx in express_methods) {
+            var method = express_methods[idx];
+            var result = Math.random()+idx;
+            srv_requires.express[method+'-rs'] = result;
+            srv_requires.express[method] = sinon.stub().returns(result);
+        }
+
+        for (var key in srv_requires) {
+            if (srv_requires.hasOwnProperty(key)) {
+                srv_requires[key]['@noCallThru'] = true;
+            }
+        }
+
+        // run server.js
+        proxyquire('../server', srv_requires);
     });
 
-    after(function(done) {
-        server.instance.close();
-        done();
+    it('mongoose config', function() {
+        assert(srv_requires.mongoose.connect.calledOnce);
+        assert(srv_requires.mongoose.connect.calledWith(env.MONGO_URI));
+
+        // auto load mongo models
+        assert(srv_requires.filewalker.calledOnce);
+        assert(srv_requires.filewalker.calledWith(
+            env.modelsPath, {matchRegExp: /.*\.js/i}
+        ));
+        assert(walker.on.calledOnce);
+        assert(walker.on.calledWith('file'));
+        assert(walker.walk.calledOnce);
+        assert(walker.walk.calledAfter(walker.on));
     });
 
-    it('should return the correct assets variable from local', function() {
-        assert.equal(app.locals.assetsURL, env.assetsURL);
+    it('express call, set views and template engine', function() {
+        assert(srv_requires.express.calledOnce);
+        assert(srv_requires.express.calledWith());
+
+        express_app.assetsURL = env.assetsURL;
+
+        assert(express_app.set.calledOnce);
+        assert(express_app.set.calledWithExactly('views', env.views));
+
+        assert(express_app.engine.calledOnce);
+        assert(express_app.engine.calledWithExactly(
+            'html', srv_requires.nunjucks.render
+        ));
     });
 
-    it('should return the correct views configuration', function() {
-        assert.equal(app.settings.views, env.views);
+    it('express configure method#use', function() {
+        var simple_exp_methods = [
+            'json',
+            'logger',
+            'favicon',
+            'session',
+            'compress',
+            'urlencoded',
+            'cookieParser'
+        ];
+        for (var idx in simple_exp_methods) {
+            var method = simple_exp_methods[idx];
+            var use_method = express_app.use.withArgs(
+                srv_requires.express[method+'-rs']
+            );
+            assert(srv_requires.express[method].calledOnce);
+            assert(use_method.calledOnce);
+            assert(use_method.calledAfter(srv_requires.express[method]));
+        }
+
+        assert(srv_requires.express.session.calledWith({secret: 'umrum-ftw'}));
     });
 
-    it('should return the correct engine', function() {
-        assert.equal(app.engines['.html'], nunjucks.render);
+    it('routes configurations', function() {
+        assert(srv_requires['./app/routes/authentication'].calledOnce);
+        assert(srv_requires['./app/routes/authentication'].calledWith(
+            express_app, srv_requires.passport
+        ));
+
+        var simple_routes = ['index', 'ping', 'dashboard', 'errors'];
+        for (var idx in simple_routes) {
+            var route = simple_routes[idx];
+            assert(srv_requires['./app/routes/'+route].calledOnce);
+            assert(srv_requires['./app/routes/'+route].calledWith(express_app));
+        }
     });
 
+    it('static routes configurations', function() {
+        assert(
+            srv_requires.express.static.withArgs(
+                env.assetsPath, {maxAge: 24*60*60*1000}
+            ).calledOnce
+        );
+        assert(
+            express_app.use.withArgs(
+                env.assetsURL, srv_requires.express['static-rs']
+            ).calledOnce
+        );
+
+        assert(
+            srv_requires.express.static.withArgs(
+                path.join(__dirname, '..', 'dist')
+            ).calledOnce
+        );
+        assert(
+            express_app.use.withArgs(
+                '/dist/', srv_requires.express['static-rs']
+            ).calledOnce
+        );
+    });
+
+    it('express listen', function() {
+        assert(express_app.listen.calledOnce);
+        assert(express_app.listen.calledWith(env.port, env.ipaddr));
+    });
+
+    it('io listen', function() {
+        assert(srv_requires['socket.io'].listen.calledOnce);
+        assert(srv_requires['socket.io'].listen.calledWith(express_listening));
+
+        assert(io.set.calledOnce);
+        assert(io.set.calledWithExactly('log level', 2));
+    });
 });
