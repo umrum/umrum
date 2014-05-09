ebf9f723fd631132798c74e48b2bfbb3d9eaf53c
Helielson <helielson@jusbrasil.com.br>
2014-05-09 01:34:15 -0300

    Fixes indentation in the dashboard view file

--- src/js/views/dashboard.js
+++ src/js/views/dashboard.js
@@ -1,4 +1,4 @@
 (function(view) {
-  var OneRumApp = window.OneRumApp;
-  OneRumApp.DashboardView = view.extend();
+    var OneRumApp = window.OneRumApp;
+    OneRumApp.DashboardView = view.extend();
 })(Ember.View)
