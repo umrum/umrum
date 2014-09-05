8b1a49bb1de5f98c8ad6223c41e8a05a4971e32b
Rafael Verger <rafael@rafaelverger.com.br>
2014-09-05 03:41:14 -0300

    Improvements and tests to middleware/authorization

--- app/config/middlewares/authorization.js
+++ app/config/middlewares/authorization.js
@@ -1,6 +1,4 @@
-/**
- * Generic require login routing middleware
- */
+
 exports.requiresLogin = function(req, res, next) {
     if (!req.isAuthenticated()) {
         return res.send(401, 'User is not authorized');
@@ -8,14 +6,9 @@ exports.requiresLogin = function(req, res, next) {
     next();
 };
 
-/**
- * User authorizations routing middleware
- */
-exports.user = {
-    hasAuthorization: function(req, res, next) {
-        if (req.profile.id != req.user.id) {
-            return res.send(401, 'User is not authorized');
-        }
-        next();
+exports.redirectAnonymous = function(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return res.redirect('/signin');
     }
+    next();
 };
