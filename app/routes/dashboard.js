9eb42804182de4cbda5e52ce6c4f78eef0d73231
Rafael Verger <rafael@rafaelverger.com.br>
2014-09-05 03:41:58 -0300

    Using authorization middleware in dashboard routes

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,64 +1,53 @@
 var api = require('../ext/redis'),
-    Site = require('../models/site');
+    Site = require('../models/site'),
+    auth = require('../config/middlewares/authorization');
 
 module.exports = function(app){
-    app.get('/dashboard', function(req, res) {
-        if ( !req.isAuthenticated() ) {
-            res.redirect('/signin');
-        } else {
-            Site.find({creator: req.user.username}, function(err, docs) {
-                if ( err ) { throw err; }
-                res.render('admin-index.html', {
+    app.get('/dashboard', auth.redirectAnonymous, function(req, res) {
+        Site.find({creator: req.user.username}, function(err, docs) {
+            if ( err ) { throw err; }
+            res.render('admin-index.html', {
+                user: req.user,
+                title: req.user.username,
+                sites: docs.map(function(e){
+                    if ( e.host ) { return {host: e.host, code:e._id}; }
+                })
+            });
+        });
+    });
+
+    app.get('/dashboard/:host', auth.redirectAnonymous, function(req, res) {
+        Site.findOne({host: req.params.host}, function (err, existent) {
+            api.getHostInfo(existent._id, function(err, info){
+                //TODO add error handler
+                res.render('admin-view.html', {
                     user: req.user,
-                    title: req.user.username,
-                    sites: docs.map(function(e){
-                        if ( e.host ) { return {host: e.host, code:e._id}; }
-                    })
+                    title: req.params.host,
+                    hostId: existent._id,
+                    data: info
                 });
             });
-        }
+        });
     });
 
-    app.get('/dashboard/:host', function(req, res) {
-        if ( !req.isAuthenticated() ) {
-            res.redirect('/signin');
-        } else {
-            Site.findOne({host: req.params.host}, function (err, existent) {
-                api.getHostInfo(existent._id, function(err, info){
-                    //TODO add error handler
-                    res.render('admin-view.html', {
-                        user: req.user,
-                        title: req.params.host,
-                        hostId: existent._id,
-                        data: info
-                    });
-                });
-            });
-        }
-    });
+    app.post('/dashboard/create', auth.requiresLogin, function(req, res) {
+        Site.findOne({host: req.body.host}, function (err, existent) {
+            if(existent) {
+                res.json({code: 500, error: 'Site is already being tracked'});
+                return;
+            }
 
-    app.post('/dashboard/create', function(req, res) {
-        if ( !req.isAuthenticated() ) {
-            res.send(401);
-        } else {
-            Site.findOne({host: req.body.host}, function (err, existent) {
-                if(existent) {
-                    res.json({code: 500, error: 'Site is already being tracked'});
+            new Site({
+                host: req.body.host,
+                creator: req.user.username,
+                created: Date.now()
+            }).save(function(err, site){
+                if ( err ) {
+                    res.json({code: 500, error: err});
                     return;
                 }
-
-                new Site({
-                    host: req.body.host,
-                    creator: req.user.username,
-                    created: Date.now()
-                }).save(function(err, site){
-                    if ( err ) {
-                        res.json({code: 500, error: err});
-                        return;
-                    }
-                    res.json({code: 200, siteCode: site._id});
-                });
+                res.json({code: 200, siteCode: site._id});
             });
-        }
+        });
     });
 };
